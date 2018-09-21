580cc2970b27ccef96bb93c1984f4c344697fb13
huanghouzhen <1165071241@qq.com>
2018-09-21 14:35:10 +0800

    loggerMessage sample code

---
+++ Fundamentals/Logging/LogerMessage/Samples/LoggerMessageSample/LoggerMessageSample/Internal/LoggerExtensions.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggerMessageSample.Internal
+{
+    public static class LoggerExtensions
+    {
+        private static readonly Action<ILogger, Exception> _indexPageRequested;
+
+        private static readonly Action<ILogger, string, Exception> _quoteAdded;
+
+        private static readonly Action<ILogger, string, int, Exception> _quoteDeleted;
+        private static readonly Action<ILogger, int, Exception> _quoteDeleteFailed;
+
+        private static Func<ILogger, int, IDisposable> _allQuotesDeletedScope;
+
+        static LoggerExtensions()
+        {
+            _indexPageRequested = LoggerMessage.Define(
+                LogLevel.Debug,
+                new EventId(1, nameof(IndexPageRequested)),
+                "Get request for Index page");
+
+            _quoteAdded = LoggerMessage.Define<string>(
+                LogLevel.Information,
+                new EventId(2, nameof(QuoteAdded)),
+                "Quote added (Quote = '{Quote}')");
+
+            _quoteDeleted = LoggerMessage.Define<string, int>(
+                LogLevel.Information,
+                new EventId(4, nameof(QuoteDeleted)),
+                "Quote deleted (Quote = '{Quote}' Id = {Id})");
+
+            _quoteDeleteFailed = LoggerMessage.Define<int>(
+                LogLevel.Error,
+                new EventId(5, nameof(QuoteDeleteFailed)),
+                "Quote delete failed (Id = {Id})");
+
+
+            _allQuotesDeletedScope = LoggerMessage.DefineScope<int>("All quotes deleted (Count = {Count})");
+        }
+
+        public static void IndexPageRequested(this ILogger logger)
+        {
+            _indexPageRequested.Invoke(logger, null);
+        }
+
+        public static void QuoteAdded(this ILogger logger, string quote)
+        {
+            _quoteAdded(logger, quote, null);
+        }
+
+        public static void QuoteDeleted(this ILogger logger, string quote, int id)
+        {
+            _quoteDeleted(logger, quote, id, null);
+        }
+
+        public static void QuoteDeleteFailed(this ILogger logger, int id, Exception ex)
+        {
+            _quoteDeleteFailed(logger, id, ex);
+        }
+
+        public static IDisposable AllQuotesDeletedScope(this ILogger logger, int count)
+        {
+            return _allQuotesDeletedScope(logger, count);
+        }
+    }
+}
--- Fundamentals/Logging/LogerMessage/Samples/LoggerMessageSample/LoggerMessageSample/Pages/Index.cshtml.cs
+++ Fundamentals/Logging/LogerMessage/Samples/LoggerMessageSample/LoggerMessageSample/Pages/Index.cshtml.cs
@@ -4,14 +4,80 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using LoggerMessageSample.Internal;
+using LoggerMessageSample.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace LoggerMessageSample.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ILogger _logger;
+        private readonly AppDbContext _db;
 
+        public IndexModel(AppDbContext db, ILogger<IndexModel> logger)
+        {
+            this._db = db;
+            this._logger = logger;
+        }
+        public async Task OnGetAsync()
+        {
+            _logger.IndexPageRequested();
+
+            Quotes = await _db.Quotes.AsNoTracking().ToListAsync();
+        }
+
+        public IList<Quote> Quotes { get; private set; }
+
+        [BindProperty]
+        public Quote Quote { get; set; }
+        public async Task<IActionResult> OnPostAddQuoteAsync()
+        {
+            _db.Quotes.Add(Quote);
+            await _db.SaveChangesAsync();
+
+            _logger.QuoteAdded(Quote.Text);
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostDeleteQuoteAsync(int id)
+        {
+            var quote = await _db.Quotes.FindAsync(id);
+
+            try
+            {
+                _db.Quotes.Remove(quote);
+                await _db.SaveChangesAsync();
+
+                _logger.QuoteDeleted(quote.Text, id);
+            }
+            catch (ArgumentNullException ex)
+            {
+                _logger.QuoteDeleteFailed(id, ex);
+            }
+
+            return RedirectToPage();
+        }
+
+
+        public async Task<IActionResult> OnPostDeleteAllQuotesAsync()
+        {
+            var quoteCount = await _db.Quotes.CountAsync();
+
+            using (_logger.AllQuotesDeletedScope(quoteCount))
+            {
+                foreach (Quote quote in _db.Quotes)
+                {
+                    _db.Quotes.Remove(quote);
+
+                    _logger.QuoteDeleted(quote.Text, quote.Id);
+                }
+                await _db.SaveChangesAsync();
+            }
+
+            return RedirectToPage();
         }
     }
 }
